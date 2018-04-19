53435a67fdba75c18248e337a6aea0a7fca3a0f6
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-04-19 17:29:04 -0300

    FIX: Corrige parametro de ordem dos resultados

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Country as CountryModel;
+use App\Http\Resources\Country as CountryResource;
+use App\Http\Resources\CountryCollection;
 use Illuminate\Http\Request;
 
 /**
@@ -15,9 +17,17 @@ class CountryController extends Controller
     /**
      * Método responsável pelo recurso da API de listagem de países.
      * @param CountryModel $countryModel Model de paises (Recebida na controller via DI)
+     * @param Request $request
+     * @return CountryResource
      */
-    public function index(CountryModel $countryModel)
+    public function index(CountryModel $countryModel, Request $request)
     {
+        $orderDescending = (bool) $request->order_descending;
 
+        $contries = $countryModel
+            ->all()
+            ->sortBy('country_code', SORT_REGULAR, $orderDescending);
+
+        return new CountryCollection($contries);
     }
 }
