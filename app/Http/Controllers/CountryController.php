113d081b9136425c5d2d0973db3439de76a86d71
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-04-19 14:02:24 -0300

    FEA: Criação da controller Country.

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Country as CountryModel;
 use Illuminate\Http\Request;
 
+/**
+ * Class CountryController
+ * @package App\Http\Controllers
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
 class CountryController extends Controller
 {
-    //
+    /**
+     * Método responsável pelo recurso da API de listagem de países.
+     * @param CountryModel $countryModel Model de paises (Recebida na controller via DI)
+     */
+    public function index(CountryModel $countryModel)
+    {
+
+    }
 }
