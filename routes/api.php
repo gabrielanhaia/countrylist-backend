d270c8f16aa4ebfe5f4dacde48dda7d65fedcf42
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2018-04-21 09:22:24 -0300

    FEA: Adiciona resposta de erro 404 da API.

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::fallback(function(){
+    return response()->json(['message' => 'Not Found!'], 404);
 });
 
 Route::get('country', ['middleware' => 'cors', 'uses' => 'CountryController@index']);
